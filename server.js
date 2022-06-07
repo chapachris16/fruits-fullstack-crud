d06591fd2df1431991c60e6eacf7f68e41e80728
christopher chapa <chapachristopher16@gmail.com>
2022-06-07 09:31:23 -0500

    created connection to database through mongoose

--- server.js
+++ server.js
@@ -7,3 +7,19 @@ const morgan = require('morgan')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const path = require('path')
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+// Setup inputs for our connect function
+const DATABASE_URL = process.env.DATABASE_URL
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+// Establish connection
+mongoose.connect(DATABASE_URL, CONFIG)
+    .on("open", () => console.log("Connected to Mongoose"))
+    .on("close", () => console.log("Disconnected from Mongoose"))
+    .on("error", (error) => console.log(error))
